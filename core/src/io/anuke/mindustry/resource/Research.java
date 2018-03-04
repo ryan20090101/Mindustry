b132a77e6e3a09010f95f9c55b8b3eb418d6406d
TheChosenEvilOne <tceo-email@protonmail.com>
2018-03-04 13:17:56 +0200

    Instant research for now

--- core/src/io/anuke/mindustry/resource/Research.java
+++ core/src/io/anuke/mindustry/resource/Research.java
@@ -9,17 +9,21 @@ public class Research {
     public static final Array<Research> researches = new Array<>();
 
     public static final Research
-            unlockOmnidrill = new Research("unlockOmnidrill"),
-            unlockFluxpump = new Research("unlockFluxpump");
+            unlockOmnidrill = new Research("unlockOmnidrill", stack(Item.dirium,2)),
+            unlockFluxpump = new Research("unlockFluxpump", stack(Item.titanium, 2));
 
     public final int id;
     public final String name;
+    public ItemStack[] requirements;
+    public String description;
     public TextureRegion region;
     public boolean researched;
 
-    public Research(String name) {
+    public Research(String name,ItemStack... requirements) {
         this.id = researches.size;
         this.name = name;
+        this.requirements = requirements;
+        this.description = Bundles.getNotNull("research."+name+".description");
 
         researches.add(this);
     }
@@ -35,5 +39,9 @@ public class Research {
     @Override
     public String toString() {
         return localizedName();
+    }
+
+    private static ItemStack stack(Item item, int amount){
+        return new ItemStack(item, amount);
     }
 }
--- core/src/io/anuke/mindustry/world/blocks/types/production/ResearchCenter.java
+++ core/src/io/anuke/mindustry/world/blocks/types/production/ResearchCenter.java
@@ -1,5 +1,111 @@
 package io.anuke.mindustry.world.blocks.types.production;
 
-public class ResearchCenter {
-    //TODO: WeaponFactory for recipes, add to recipe list after you research?
+import io.anuke.mindustry.Vars;
+import io.anuke.mindustry.net.Net;
+import io.anuke.mindustry.net.NetEvents;
+import io.anuke.mindustry.resource.*;
+import io.anuke.mindustry.world.Block;
+import io.anuke.mindustry.world.Tile;
+import io.anuke.ucore.core.Effects;
+import io.anuke.ucore.function.Listenable;
+import io.anuke.ucore.graphics.Draw;
+import io.anuke.ucore.scene.style.TextureRegionDrawable;
+import io.anuke.ucore.scene.ui.ImageButton;
+import io.anuke.ucore.scene.ui.Tooltip;
+import io.anuke.ucore.scene.ui.layout.Table;
+
+import static io.anuke.mindustry.Vars.*;
+
+public class ResearchCenter extends Block{
+
+    public ResearchCenter(String name){
+        super(name);
+        solid = true;
+        destructible = true;
+    }
+
+    @Override
+    public boolean isConfigurable(Tile tile){
+        return !Vars.android;
+    }
+
+    @Override
+    public void buildTable(Tile tile, Table table) {
+        int i = 0;
+
+        Table content = new Table();
+
+        for(Research res : Research.researches){
+
+            ItemStack[] requirements = res.requirements;
+
+            Table tiptable = new Table();
+
+            Listenable run = ()->{
+                tiptable.clearChildren();
+
+                String description = res.description;
+
+                tiptable.background("pane");
+                tiptable.add("[orange]" + res.localizedName(), 0.5f).left().padBottom(2f);
+
+                Table reqtable = new Table();
+
+                tiptable.row();
+                tiptable.add(reqtable).left();
+
+                if(!world.getResearchStatus(res){
+                    for(ItemStack s : requirements){
+
+                        int amount = Math.min(state.inventory.getAmount(s.item), s.amount);
+                        reqtable.addImage(s.item.region).padRight(3).size(8*2);
+                        reqtable.add(
+                                (amount >= s.amount ? "" : "[RED]")
+                                        + amount + " / " +s.amount, 0.5f).left();
+                        reqtable.row();
+                    }
+                }
+
+                tiptable.row();
+                tiptable.add().size(4);
+                tiptable.row();
+                tiptable.add("[gray]" + description).left();
+                tiptable.row();
+                if(world.getResearchStatus(res)){
+                    tiptable.add("$text.researched").padTop(4).left();
+                }
+                tiptable.margin(8f);
+            };
+
+            run.listen();
+
+            Tooltip<Table> tip = new Tooltip<>(tiptable, run);
+
+            tip.setInstant(true);
+
+            ImageButton button = content.addImageButton("white", 8*4, () -> {
+                state.inventory.removeItems(requirements);
+                world.research(res);
+                ui.hudfrag.updateWeapons();
+                run.listen();
+                Effects.sound("purchase");
+
+                /** TODO: ADD OWN NETEVENT FOR UPGRADING
+                *if(Net.client()){
+                *    NetEvents.handleUpgrade(res);
+                *}
+                */
+            }).size(49f, 54f).padBottom(-5).get();
+
+            button.setDisabled(() -> world.getResearchStatus(res) || !state.inventory.hasItems(requirements));
+            button.getStyle().imageUp = new TextureRegionDrawable(res.region);
+            button.addListener(tip);
+
+            if(++i % 3 == 0){
+                content.row();
+            }
+        }
+
+        table.add(content).padTop(140f);
+    }
 }
